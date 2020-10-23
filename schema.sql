b965d267ebba571c7ee3535f4a3c12d7975fa77a
Christi Kerr <christinakerr394@gmail.com>
2020-10-23 15:58:16 -0500

    Added MySQL schema

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE departments(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+	FOREIGN KEY(role_id) REFERENCES roles(id),
+	FOREIGN KEY(manager_id) REFERENCES employees(id)
+);
